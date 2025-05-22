a255679d4e80b5246666901dd7f44141c3b89d5e
Eirik Stanghelle Morland <eirik@morland.no>
2025-05-22 20:48:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,8 @@ settings = DropboxSettings()  # type: ignore[call-arg]
 app = FastAPI()
 
 @app.get("/")
-def hello_world():
+def hello_world() -> dict[str, str]:
     return {'message': 'Hello from FastAPI'}
-
-
-@app.get("/hello/{name}")
-def hello(name: str):
-    return {"message": f'Hello from FastAPI, {name}!'}
 
 @app.get("/battery")
 def battery() -> dict[str, str]:
@@ -21,9 +16,9 @@ def battery() -> dict[str, str]:
     return {"battery": value + "%"}
 
 @app.post("/battery")
-def battery_post(level: int):
+def battery_post(level: int) -> dict[str, str]:
     db = DropboxDB(settings.dropbox_refresh_token)
-    value: str = db.set("battery", str(level))
+    db.set("battery", str(level))
     return battery()
 
 handler = Mangum(app)
