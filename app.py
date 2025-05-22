8f75f950ecc1e11c8facc93b538f4c25f68ac628
Eirik S. Morland <eirik@morland.no>
2025-05-22 18:10:00 +0200

    Dropbox read and write key/val

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from db import DropboxDB, DropboxSettings
+settings = DropboxSettings()
 
 app = FastAPI()
 
@@ -11,5 +13,17 @@ def hello_world():
 @app.get("/hello/{name}")
 def hello(name: str):
     return {"message": f'Hello from FastAPI, {name}!'}
+
+@app.get("/battery")
+def battery():
+    db = DropboxDB(settings.dropbox_refresh_token)
+    value: str = db.get("battery")
+    return {"battery": value + "%"}
+
+@app.post("/battery")
+def battery_post(level: int):
+    db = DropboxDB(settings.dropbox_refresh_token)
+    value: str = db.set("battery", str(level))
+    return battery()
 
 handler = Mangum(app)
